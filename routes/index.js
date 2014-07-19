3c826040bf30de1567f1f6ba1d09d6927a475d15
azurelogic <azurelogic@gmail.com>
2014-07-19 15:38:37 -0400

    Change title. Add scripts to layout.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Adopt-A-Lot South Bend' });
 });
 
 router.get('/volunteer/claim', require('../controllers').volunteer.claim.get);
