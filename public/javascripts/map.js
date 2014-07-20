326f9d1b570f5078cdcc7e1f4452d4f281f70389
Nathan Smith <nathanrandal@hotmail.com>
2014-07-19 23:35:39 -0400

    added map popup for entry when clicked with basic entry information

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,17 +8,47 @@ $(window).load(function() {
       zoom: 14,
       center: center
     }
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    infoWindow = new google.maps.InfoWindow();
+
+
+
+    var EntryModel = Backbone.Model.extend({
+        infoWindowTemplate: _.template($('#map_entry_info_window_template').html()),
+        idAttribute: 'state_parcel_id',
+
+        // Display the entry info in the map InfoWindow
+        displayInfoWindow: function(marker) {
+            infoWindow.setContent(this.infoWindowTemplate(this.toJSON()));    // render template
+            infoWindow.open(map, marker);
+        }
+    });
+
+
+    siteData = new Backbone.Collection([], {model: EntryModel});
+
+
+
+
+
 
     // Retrieve our data and plot it
     $.getJSON(url, function(data, textstatus) {
-          console.log(data);
+          console.log('Data retrieved');
+          siteData.add(data);
+
           $.each(data, function(i, entry) {
               var marker = new google.maps.Marker({
                   position: new google.maps.LatLng(entry.location_1.latitude,
                                                    entry.location_1.longitude),
                   map: map,
                   title: location.name
+              });
+              marker.set('id', entry.state_parcel_id);    // Set data entry id
+
+              // Display info for entry on marker click
+              google.maps.event.addListener(marker, 'click', function() {
+                  siteData.get(marker.id).displayInfoWindow(marker);
               });
           });
     });
