e34f1b542be0bfe957acee7cbe09a59acc9d6671
Nathan Smith <nathanrandal@gmail.com>
2015-01-22 00:31:40 -0500

    working on list pagination

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,9 @@
 function initialize() {
+  var limit = 100;
+  var page = 1;
+  fetchNext = function() {
+    lots.fetch({data: {limit: limit, skip: (page++)*limit}});
+  }
 
   lots = new adoptalot.LotCollection();
   mapview = new adoptalot.MapView({lots: lots});
@@ -6,7 +11,13 @@ function initialize() {
 
   mapview.render();
   lotslist.render();
-  lots.fetch({data: {limit: 500}});
+  // lots.fetch({data: {limit: 100, skip: 0}});
+  fetchNext();
+
+  $('#next_page').click(function(e) {
+    e.preventDefault();
+    fetchNext();
+  });
 }
 
 $(document).ready(initialize);
