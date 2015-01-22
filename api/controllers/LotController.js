0f949217db71b9a6fdc4ad48d4b8776b7f27b74d
Nathan Smith <nathanrandal@gmail.com>
2015-01-21 21:54:54 -0500

    added bower & updated readme

---
+++ api/controllers/LotController.js
@@ -0,0 +1,11 @@
+/**
+ * LotController
+ *
+ * @description :: Server-side logic for managing lots
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
